f1789b61d881257a52e99980533f478e4f4fe20a
solunska <a_solunska@outlook.com>
2024-11-09 14:15:01 +0100

    implemented data fetching

---
+++ 04. TV Shows App/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import BreakingBadCards from './components/BreakingBadCards.vue';
+</script>
+
+<template>
+  <main>
+    <div class="hero">
+      <h1>Rick and Morty Charachters</h1>
+    </div>
+    <Suspense>
+      <template #default>
+        <BreakingBadCards />
+      </template>
+      <template #fallback>
+        <div>
+          <p>Loading...</p>
+        </div>
+      </template>
+    </Suspense>
+  </main>
+</template>
+
+<style scoped>
+.hero {
+  height: 40vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  flex-direction: column
+}
+
+.hero img {
+  position: absolute;
+  height: 700px;
+  opacity: 0.1;
+}
+
+.hero h1 {
+  font-size: 100px;
+  font-weight: bold;
+  color: rgb(27, 26, 26);
+}
+
+.hero p {
+  cursor: pointer;
+}
+</style>
---
+++ 04. TV Shows App/src/components/Card.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { defineProps } from 'vue';
+const { image, name, species } = defineProps(['image', 'name', 'species'])
+</script>
+
+<template>
+    <n-card :title="name">
+        <template #cover>
+            <img :src="image">
+        </template>
+        <h3>{{ species }}</h3>
+    </n-card>
+</template>
+
+<style scoped>
+.n-card {
+    width: 200px;
+    margin: 10px 20px;
+
+}
+
+.n-card img {
+    height: 250px
+}
+
+p {
+    font-size: 10px;
+}
+
+</style>
